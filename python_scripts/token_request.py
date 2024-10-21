4e7bc04020834c80f079d2b3de38b62ac15d1807
Guilherme Lazari <guilherme.lazari@programmers.com.br>
2024-10-21 08:47:47 -0300

    Auto-generated token

--- python_scripts/token_request.py
+++ python_scripts/token_request.py
@@ -2,8 +2,20 @@ import requests
 import base64
 from dotenv import load_dotenv
 import os
+env_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env')
 
-load_dotenv()
+def update_env_file(key, value):
+    with open(env_path, 'r') as file:
+        lines = file.readlines()
+
+    with open(env_path, 'w') as file:
+        for line in lines:
+            if line.startswith(key):
+                file.write(f"{key}={value}\n")
+            else:
+                file.write(line)
+
+load_dotenv(env_path)
 
 CLIENT_ID = os.getenv('CLIENT_ID')
 CLIENT_SECRET = os.getenv('CLIENT_SECRET')
@@ -25,6 +37,9 @@ response = requests.post(auth_url, headers=headers, data=data)
 if response.status_code == 200:
     token = response.json().get("access_token")
     print(f"Token de acesso: {token}")
+
+    update_env_file("SPOTIFY_API_KEY", f"'{token}'")
+    
 else:
     print(f"Erro na autenticação. Status Code: {response.status_code}")
     print(response.json())
